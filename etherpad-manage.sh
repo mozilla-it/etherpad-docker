479a49e58e7870d6906d7c64b632389e2e7bdcee
Christina Harlow <charlow@mozilla.com>
2021-09-01 11:28:43 -0400

    revert etherpad manage bash script to working state

--- etherpad-manage.sh
+++ etherpad-manage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -euxo pipefail
+
 min_stamp=$(date -d "-15 days" +%s)
 
 count=0
@@ -13,8 +13,9 @@ for p in $(curl -s --data-urlencode "apikey=$ETHERPAD_API_KEY" "http://localhost
 			--data-urlencode "apikey=$ETHERPAD_API_KEY" \
 			--data-urlencode "padID=$p" \
 			"http://localhost:$PORT/api/1/deletePad"
-		count = $(( $count + 1 ))
+		((count++))
 	fi
 done
 
-echo "Identified and deleted $count pads"
+echo "Identified $count pads for removal"
+
